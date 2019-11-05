08f6a6a6fd043269b0c3d11532b55a85117a5a8f
AndruhaPower <pazit@yandex.ru>
2019-11-06 00:49:31 +0300

    Wrote 1st screen today.
    -no storyboard

---
+++ Millionaire-Quiz/Millionaire-Quiz/CustomMainView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomMainView.swift
+//  Millionaire-Quiz
+//
+//  Created by Andrew on 05/11/2019.
+//  Copyright © 2019 Andrew. All rights reserved.
+//
+
+import UIKit
+
+class CustomMainView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Millionaire-Quiz/Millionaire-Quiz/LoadImageOperation.swift
@@ -0,0 +1,9 @@
+//
+//  LoadImageOperation.swift
+//  Millionaire-Quiz
+//
+//  Created by Andrew on 05/11/2019.
+//  Copyright © 2019 Andrew. All rights reserved.
+//
+
+import Foundation
